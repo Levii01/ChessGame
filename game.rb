73f075d4b1e52cd6ad556376c10d48c229ad042d
Levii01 <m.sedziak01@gmail.com>
2016-10-04 22:48:43 +0200

    Add bishop.rb and modification game.rb

--- game.rb
+++ game.rb
@@ -2,6 +2,8 @@ require_relative 'figure'
 require_relative 'knight'
 require_relative 'king'
 require_relative 'rook'
+require_relative 'bishop'
 #print Knight.new("g2").possible_moves
 #print King.new("g2").possible_moves
-print Rook.new("g2").possible_moves
+#print Rook.new("g2").possible_moves
+#print Bishop.new("g2").possible_moves
--- rook.rb
+++ rook.rb
@@ -1,6 +1,6 @@
 class Rook < Figure
 private
-    def vectors
+    def self.vectors
         ((-7..-1).to_a + (1..7).to_a).map do |i|
             [[i,0],[0,i]]
         end.flatten(1)
