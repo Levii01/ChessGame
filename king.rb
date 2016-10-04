f84e62eb8942399e94053d57268ff523666959ce
Levii01 <m.sedziak01@gmail.com>
2016-10-04 21:55:49 +0200

    Add figure, game, rook and clean up code

--- king.rb
+++ king.rb
@@ -1,35 +1,7 @@
-class King
-    attr_reader :x, :y
-
-    def initialize(move)
-      @x = letters.index(move[0])
-      @y = move[1].to_i
-    end
-
-    def possible_moves
-      moves.map { |out_x , out_y| "#{letters[out_x]}#{out_y}" }
-    end
-
-    def all_moves
-      vectors.map{|wx , wy| [wx + x , wy + y]}
-    end
-
-    def onboard(position)
-      (1..8).include?(position)
-    end
-
-    def moves
-      all_moves.sort.select { |_x , _y| onboard(_x) && onboard(_y) }
-    end
-
-    def letters
-      " abcdefgh"
-    end
+class King < Figure
+    private
 
     def vectors
       [[-1, 1], [-1, 0], [-1, -1], [0, 1], [0, -1], [1, 1], [1, 0], [1, -1]]
     end
 end
-
-a = King.new("g2").possible_moves
-print a
--- knight.rb
+++ knight.rb
@@ -1,36 +1,6 @@
-class Knight
-    attr_reader :x, :y
-
-    def initialize(move)
-      @x = letters.index(move[0])
-      @y = move[1].to_i
-    end
-
-    def possible_moves
-      moves.map! { |out_x , out_y| "#{letters[out_x]}#{out_y}" }
-    end
-
+class Knight < Figure
 private
-
-    def all_moves
-      vectors.map{|wx , wy| [wx + x , wy + y]}
-    end
-
-    def onboard(position)
-      (1..8).include?(position)
-    end
-
-    def moves
-      all_moves.sort.select! { |_x , _y| onboard(_x) && onboard(_y) }
-    end
-
-    def letters
-      " abcdefgh"
-    end
-
-    def vectors
+    def self.vectors
       [[-1, 2], [1, 2], [-1, -2], [1, -2], [-2, 1], [-2, -1], [2, 1], [2, -1]]
     end
 end
-
-print Knight.new("g2").possible_moves
